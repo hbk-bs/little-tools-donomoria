572ffa9ca7ae750c9260b4da0df0c0ba6d02276f
donomoria <m.kukharenka@hbk-bs.de>
2023-11-29 16:39:09 +0300

    finally i can draw

--- sketch.js
+++ sketch.js
@@ -1,22 +1,48 @@
+const saveButton = document.querySelector('button#save');
+const colorInput = document.querySelector('input#color');
+console.log(colorInput);
+
+let paintColor = '#ff6347';
+colorInput.value = paintColor;
+
 function setup() {
-    createCanvas(500,500);
-}
+    const canvas = createCanvas(500,500);
+    canvas.parent ('sketch');
+    console.log('saveButton');
+
+    background (255);
+    saveButton.addEventListener ("click",()=>{
+
+        console.log('clicked');
+        save('image.png');
+    })
+    colorInput.addEventListener("input", ()=>{
+        console.log(colorInput.value);
+        paintColor = colorInput.value;
+    })
+
+    };
+    
 
 function draw() {
 }
 
 function mouseDragged() {
+    fill(paintColor);
  circle(mouseX, mouseY,10);
  line(pmouseX, pmouseY, mouseX, mouseY);
  for (let i = 0; i< 100; i++){
     point(mouseX + random(-10,10),
     mouseY + random(-10,10),)
  }
-
+function mouseDragged(){
+    save('image.png')
+}
 }
 
+
 function KeyPressed(){
-    if (key=== 's'){
+    if (key === 's'){
         save('image.png');
     }
  }
