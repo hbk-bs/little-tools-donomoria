4169da99b927b783f6b096c84f4d2dd35733762e
donomoria <m.kukharenka@hbk-bs.de>
2024-01-07 07:23:51 +0300

    newwww

--- sketch.js
+++ sketch.js
@@ -6,43 +6,50 @@ let paintColor = '#ff6347';
 colorInput.value = paintColor;
 
 function setup() {
-    const canvas = createCanvas(500,500);
-    canvas.parent ('sketch');
-    console.log('saveButton');
+    createCanvas(windowWidth * 0.48, windowHeight * 0.48).parent('sketch');
+    background(255);
 
-    background (255);
-    saveButton.addEventListener ("click",()=>{
-
+    saveButton.addEventListener("click", () => {
         console.log('clicked');
         save('image.png');
-    })
-    colorInput.addEventListener("input", ()=>{
+    });
+
+    colorInput.addEventListener("input", () => {
         console.log(colorInput.value);
         paintColor = colorInput.value;
-    })
-
-    };
-    
+    });
+}
 
 function draw() {
 }
 
 function mouseDragged() {
     fill(paintColor);
- circle(mouseX, mouseY,10);
- line(pmouseX, pmouseY, mouseX, mouseY);
- for (let i = 0; i< 100; i++){
-    point(mouseX + random(-5,5),
-    mouseY + random(-5,5),)
- }
-function mouseDragged(){
-    save('image.png')
-}
+
+    let starSize = 30;
+    beginShape();
+    for (let i = 0; i < 5; i++) {
+        let angle = TWO_PI * i / 5 - HALF_PI;
+        let x = mouseX + cos(angle) * starSize;
+        let y = mouseY + sin(angle) * starSize;
+        vertex(x, y);
+
+        angle += TWO_PI / 10;
+        x = mouseX + cos(angle) * (starSize / 2);
+        y = mouseY + sin(angle) * (starSize / 2);
+        vertex(x, y);
+    }
+    endShape(CLOSE);
 }
 
-
-function KeyPressed(){
-    if (key === 's'){
+function keyPressed() {
+    if (key === 's' || key === 'S') {
         save('image.png');
     }
- }
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth * 0.48, windowHeight * 0.48);
+}
+
+window.addEventListener('resize', windowResized);
